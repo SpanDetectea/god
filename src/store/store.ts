5232e4715db6dfd697746cb7f6ba7f80a07a7cea
SpanDetectea <spandetectea@gmail.com>
2025-08-09 12:05:57 +0300

    Настройка проекта

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from '@/store/slices/usersSlices'
+
+export const store = configureStore({
+  reducer: {
+    users: usersReducer
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
